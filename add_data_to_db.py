b6e874e6c55d4c3560c44644051965d740663c1b
Oleg_Khalyvin <olegkhalyvin@gmail.com>
2023-08-27 19:37:03 +0700

    update add db2

---
+++ add_data_to_db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+vacs = pd.read_csv('data/vacs.csv')
+skills = pd.read_csv('data/skills.csv')
+engine = create_engine("postgresql://admin:admin@localhost:5432/analytics_hh")
+
+vacs.to_sql("vacancies", engine, if_exists="append", index=False)
+skills.to_sql("skill_types", engine, if_exists="append", index=False)
