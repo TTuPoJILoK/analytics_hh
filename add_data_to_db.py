e5405e656eeea51f00f9e30eccf6cc40581a7bda
oleg <oleglhalyvin@gmail.com>
2023-10-10 15:28:00 +0700

    update requirements and gitignore

--- add_data_to_db.py
+++ add_data_to_db.py
@@ -4,10 +4,10 @@ import os
 from dotenv import load_dotenv
 
 # Скрипт загружает  2 csv файла  для инициализации базы данных на сервере
-vacs = pd.read_csv('data/vacs_new.csv')
-skills = pd.read_csv('data/skills_new.csv')
+vacs = pd.read_csv('data_new/vacs.csv')
+skills = pd.read_csv('data_new/skills.csv')
 load_dotenv()
 engine = create_engine(os.getenv('POSTGRES_URL_LOCALHOST'))
 
 vacs.to_sql("vacancies", engine, if_exists="append", index=False)
-skills.to_sql("skill_types", engine, if_exists="append", index=False)
+skills.to_sql("skills", engine, if_exists="append", index=False)
